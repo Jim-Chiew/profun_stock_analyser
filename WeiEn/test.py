689add064413fe26dc2f7b1b5312b50f6114b5ae
TheAALiens <weienoh@gmail.com>
2025-09-05 12:41:56 +0800

    init

---
+++ WeiEn/test.py
@@ -0,0 +1,7 @@
+from ipywidgets import interact_manual
+
+@interact_manual(period=['1mo', '3mo', '6mo'], interval=['1d', '1h'])
+def interactive_amzn(period='1mo', interval='1d'):
+    prices, dates = get_stock_prices('AMZN', period, interval)
+    trade = best_time_to_buy_and_sell(prices)
+    plot_prices_with_trade(prices, dates, trade, 'AMZN')
