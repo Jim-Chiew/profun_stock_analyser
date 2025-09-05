0a6e4f35b3fefd962b5be38ddfaa8ad5eb6c5916
TheAALiens <weienoh@gmail.com>
2025-09-05 12:49:15 +0800

    testing commit by changing AMZN to AAPL

--- WeiEn/test.py
+++ WeiEn/test.py
@@ -2,6 +2,7 @@ from ipywidgets import interact_manual
 
 @interact_manual(period=['1mo', '3mo', '6mo'], interval=['1d', '1h'])
 def interactive_amzn(period='1mo', interval='1d'):
-    prices, dates = get_stock_prices('AMZN', period, interval)
+    prices, dates = get_stock_prices('AAPL', period, interval)
     trade = best_time_to_buy_and_sell(prices)
-    plot_prices_with_trade(prices, dates, trade, 'AMZN')
+    plot_prices_with_trade(prices, dates, trade, 'AAPL')
+#testing commit
